3897037febdb81e547d9f562a53027f792bb3f1a
hundredvisionsguy <cwinikka@gmail.com>
2021-01-25 21:10:55 -0800

    adding menu toggle feature

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 /* Scripts:
     responsive navbar: https://itnext.io/how-to-build-a-responsive-navbar-using-flexbox-and-javascript-eb0af24f19bf
     */
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+});
